807aabb05eca882cad325c2aa77cdba7a18aecb0
PratPanig <miku.prateek@gmail.com>
2017-06-02 19:00:11 -0500

    Adding more files

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.notification.Failure;
 public class TestRunner {
 
 	public static void main(String[] args){
-		Result result=JUnitCore.runClasses(TestingJunit.class);
+		Result result=JUnitCore.runClasses(JunitTestSuite.class);
 		for(Failure failure:result.getFailures())
 			System.out.println(failure.toString());
 		System.out.println("Result=="+result.wasSuccessful());
